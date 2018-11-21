fb2b1252cba418c2a92cef02a20035386c15fe3f
PrettyWood <eric.jolibois@toucantoco.com>
2018-11-21 18:06:22 +0100

    helpers

--- peakina/helpers.py
+++ peakina/helpers.py
@@ -1,6 +1,8 @@
 import csv
+import inspect
 import mimetypes
 from itertools import islice
+from typing import Optional
 
 import chardet
 
@@ -17,7 +19,7 @@ def str_head(file_path: str, n: int, encoding: str = None) -> str:
         return ''.join(line for line in islice(f, n))
 
 
-def validate_encoding(file_path: str, encoding: str) -> bool:
+def validate_encoding(file_path: str, encoding: str = None) -> bool:
     """Detect encoding of a file based on its 100 first lines"""
     with open(file_path, encoding=encoding) as f:
         try:
@@ -37,20 +39,57 @@ def detect_sep(file_path: str, encoding: str = None):
     return csv.Sniffer().sniff(str_head(file_path, 100, encoding)).delimiter
 
 
-def detect_type(file_path: str) -> str:
-    supported_mimetypes = {
-        'text/csv': 'csv',
-        'application/vnd.ms-excel': 'excel',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'excel',
-        'application/json': 'json',
-    }
+SUPPORTED_MIMETYPES = {
+    'text/csv': 'csv',
+    'text/tab-separated-values': 'csv',
+    'application/vnd.ms-excel': 'excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'excel',
+    'application/json': 'json',
+}
+SUPPORTED_TYPES = sorted(set(SUPPORTED_MIMETYPES.values()))
+
+
+def detect_type(file_path: str) -> Optional[str]:
+    """
+    Detect type of `file_path`.
+    Returns None if the type couldn't be guessed
+    """
     mimetype, _ = mimetypes.guess_type(file_path)
+    if mimetype is None:
+        return None
     try:
-        return supported_mimetypes[mimetype]
+        return SUPPORTED_MIMETYPES[mimetype]
     except KeyError:
-        supported_types = sorted(set(supported_mimetypes.values()))
-        raise UnknownType(f'Unknown type. Supported types are: {", ".join(supported_types)}')
+        raise UnknownType('Unknown detected type')
+
+
+def guess_type(file_path: str, is_regex: bool) -> Optional[str]:
+    """
+    Try to guess the type of `file_path`
+    Returns None in case of generic extension (is_regex=True and file_path=`...*')
+    """
+    if is_regex:
+        file_path = file_path.rstrip('$')
+    detected_type = detect_type(file_path)
+    if is_regex and detected_type is None:
+        return None
+    if detected_type not in SUPPORTED_TYPES:
+        raise UnknownType(f'Unknown guessed type {detected_type!r}')
+    return detected_type
+
+
+def validate_kwargs(kwargs: dict, methods: list) -> bool:
+    """Validate that kwargs are at least in one signature of the methods"""
+    allowed_kwargs = {kw for method in methods for kw in inspect.signature(method).parameters}
+    bad_kwargs = set(kwargs) - allowed_kwargs
+    if bad_kwargs:
+        raise ValueError(f'Unsupported kwargs: {", ".join(map(repr, bad_kwargs))}')
+    return True
 
 
 class UnknownType(Exception):
     """raised when type is unknown"""
+
+    def __init__(self, message):
+        message = f'{message}. Supported types are: {", ".join(map(repr, SUPPORTED_TYPES))}.'
+        super().__init__(message)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,8 +6,10 @@ from peakina.helpers import (
     detect_encoding,
     detect_sep,
     detect_type,
+    guess_type,
     str_head,
     validate_encoding,
+    validate_kwargs,
 )
 
 
@@ -44,4 +46,28 @@ def test_detect_type(path):
     assert detect_type(path('0_0.csv')) == 'csv'
     with raises(UnknownType) as e:
         detect_type(path('fixture.xml'))
-    assert str(e.value) == 'Unknown type. Supported types are: csv, excel, json'
+    assert str(e.value) == "Unknown detected type. Supported types are: 'csv', 'excel', 'json'."
+
+
+def test_guess_type():
+    assert guess_type('a.tsv', is_regex=False) == 'csv'
+    with raises(UnknownType) as e:
+        guess_type('a.tsv$', is_regex=False)
+    assert str(e.value) == "Unknown guessed type None. Supported types are: 'csv', 'excel', 'json'."
+    with raises(UnknownType):
+        guess_type('a.jpg', is_regex=False)
+    with raises(UnknownType):
+        guess_type('a.jpg$', is_regex=True)
+    assert guess_type('a.tsv', is_regex=True) == 'csv'
+    assert guess_type('a.tsv$', is_regex=True) == 'csv'
+    assert guess_type('a.*', is_regex=True) is None
+
+
+def test_validate_kwargs():
+    import pandas as pd
+
+    assert validate_kwargs({'encoding': 'utf8'}, [pd.read_csv])
+    with raises(ValueError) as e:
+        validate_kwargs({'sheet_name': 0}, [pd.read_csv])
+    assert str(e.value) == "Unsupported kwargs: 'sheet_name'"
+    assert validate_kwargs({'sheet_name': 0}, [pd.read_csv, pd.read_excel])
