10b9551c0b128e97c505fe57c842945dcbbf59b5
fspot <fred@fspot.org>
2019-06-26 11:51:54 +0200

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,12 @@ def ftp_path():
     if not ftp:
         pytest.skip("'FTP_PATH' is not set")
     return ftp
+
+
+@pytest.fixture(scope='module')
+def http_path():
+    return (
+        'https://gist.githubusercontent.com/armgilles/'
+        '194bcff35001e7eb53a2a8b441e8b2c6/raw/'
+        '92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv'
+    )
