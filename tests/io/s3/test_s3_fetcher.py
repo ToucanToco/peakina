7a4b7aafd8f60ab752dd9b1966b978b609c5e027
PrettyWood <em.jolibois@gmail.com>
2020-03-19 11:23:56 +0100

    test s3_open on s3 server docker

--- tests/io/s3/test_s3_fetcher.py
+++ tests/io/s3/test_s3_fetcher.py
@@ -1,39 +1,49 @@
-from contextlib import suppress
+import boto3
+from pytest import fixture, raises
 
-from pytest import raises
-
-import peakina as pk
 from peakina.io.s3.s3_fetcher import S3Fetcher
 
 
-def test_s3_fetcher(mocker):
-    dirpath = 's3://aws_key:aws_secret@bucketname'
-    filepath = f'{dirpath}/objectname'
+@fixture(scope='module')
+def s3_container(service_container):
+    def check(host_port):
+        session = boto3.session.Session()
+        s3_url = f'http://localhost:{host_port}'
+        s3_client = session.client(
+            service_name='s3',
+            aws_access_key_id='accessKey1',
+            aws_secret_access_key='verySecretKey1',
+            endpoint_url=s3_url,
+        )
+        s3_client.list_buckets()
 
-    # TODO: Test on real s3 like ftp
-    mocker.patch('peakina.io.s3.s3_fetcher.s3_open').return_value = 'pika'
-    assert S3Fetcher.open(filepath) == 'pika'
+    return service_container('s3', check)
+
+
+@fixture(scope='module')
+def s3_endpoint_url(s3_container):
+    session = boto3.session.Session()
+    s3_url = f'http://localhost:{s3_container["port"]}'
+    s3_client = session.client(
+        service_name='s3',
+        aws_access_key_id='accessKey1',
+        aws_secret_access_key='verySecretKey1',
+        endpoint_url=s3_url,
+    )
+    s3_client.create_bucket(Bucket='mybucket')
+    s3_client.upload_file('tests/fixtures/0_0.csv', 'mybucket', '0_0.csv')
+    s3_client.upload_file('tests/fixtures/0_1.csv', 'mybucket', '0_1.csv')
+    return s3_url
+
+
+def test_s3_fetcher(s3_endpoint_url):
+    dirpath = 's3://accessKey1:verySecretKey1@mybucket'
+    filepath = f'{dirpath}/0_0.csv'
+
+    with S3Fetcher.open(filepath, client_kwargs={'endpoint_url': s3_endpoint_url}) as f:
+        assert f.read() == b'a,b\n0,0\n0,1'
 
     with raises(NotImplementedError):
         S3Fetcher.listdir(dirpath)
     with raises(NotImplementedError):
         S3Fetcher.mtime(filepath)
-
-
-def test_s3_fetcher_custom_endpoint(mocker):
-
-    m = mocker.patch('peakina.io.s3.s3_utils.s3fs.S3FileSystem')
-
-    dirpath = 's3://aws_key:aws_secret@bucketname'
-    filepath = f'{dirpath}/objectname.csv'
-    with suppress(TypeError):
-        pk.read_pandas(
-            filepath,
-            fetcher_kwargs={'client_kwargs': {'endpoint_url': 'https://internal.domain:8080/truc'}},
-        )
-
-    kwargs = m.call_args[1]
-    assert 'key' in kwargs
-    assert 'secret' in kwargs
-    assert 'client_kwargs' in kwargs
-    assert 'endpoint_url' in kwargs['client_kwargs']
