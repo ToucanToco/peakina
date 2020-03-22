29b58dddd7042ac7a31ac8c04cb30886df045d58
PrettyWood <em.jolibois@gmail.com>
2020-03-22 18:55:12 +0100

    Refacto on fetcher to pass fetcher_kwargs directly in the __init__

--- peakina/io/ftp/ftp_fetcher.py
+++ peakina/io/ftp/ftp_fetcher.py
@@ -1,5 +1,5 @@
 import os
-from typing import BinaryIO, List, Optional
+from typing import BinaryIO, Dict, List, Optional
 
 from ..fetcher import Fetcher, register
 from .ftp_utils import FTP_SCHEMES, dir_mtimes, ftp_mtime, ftp_open
@@ -7,22 +7,30 @@ from .ftp_utils import FTP_SCHEMES, dir_mtimes, ftp_mtime, ftp_open
 
 @register(schemes=FTP_SCHEMES)
 class FTPFetcher(Fetcher):
-    def open(self, filepath, **fetcher_kwargs) -> BinaryIO:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._mtimes_cache: Dict[str, Dict[str, Optional[int]]] = {}
+
+    def get_dir_mtimes(self, dirpath: str) -> Dict[str, Optional[int]]:
+        if dirpath not in self._mtimes_cache:
+            self._mtimes_cache[dirpath] = dir_mtimes(dirpath)
+        return self._mtimes_cache[dirpath]
+
+    def open(self, filepath) -> BinaryIO:
         return ftp_open(filepath)
 
-    def listdir(self, dirpath, **fetcher_kwargs) -> List[str]:
+    def listdir(self, dirpath) -> List[str]:
         """
         Make use of listdir to get all mtimes of remote files at once and
         put this information in cache.
         This will save us time by avoiding to create a new FTP connection
         for each file when we'll want to get its mtime.
         """
-        self._mtimes_cache = dir_mtimes(dirpath)
-        return list(self._mtimes_cache.keys())
+        return list(self.get_dir_mtimes(dirpath).keys())
 
-    def mtime(self, filepath, **fetcher_kwargs) -> Optional[int]:
-        filename = os.path.basename(filepath)
-        if hasattr(self, '_mtimes_cache') and filename in self._mtimes_cache:
-            return self._mtimes_cache[filename]
+    def mtime(self, filepath) -> Optional[int]:
+        dirpath, filename = os.path.split(filepath)
+        if dirpath in self._mtimes_cache:
+            return self._mtimes_cache[dirpath][filename]
         else:
             return ftp_mtime(filepath)
