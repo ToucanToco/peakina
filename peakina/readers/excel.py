59a4d68fb81c07bedabe563df73028b60b577151
sanix-darker <s4nixd@gmail.com>
2022-02-09 17:21:33 +0100

    feat: add generator for xldr reader+ offset/limit

--- peakina/readers/excel.py
+++ peakina/readers/excel.py
@@ -2,13 +2,28 @@
 Module to add xml support
 """
 from io import StringIO
-from itertools import islice
 from typing import Any, Dict
 
 import pandas as pd
 import xlrd
 from openpyxl import load_workbook
 from openpyxl.utils.exceptions import InvalidFileException
+
+
+def _yielder(preview: bool, s: Any, nrows: int = 2, offset: int = 0) -> Any:
+    """
+    A generator for old excel types
+    """
+    if preview:
+        to_iter = (offset, offset + nrows)
+    else:
+        to_iter = (0, s.nrows)
+
+    for rx in range(to_iter[0], to_iter[1]):
+        try:
+            yield s.row(rx)
+        except Exception:
+            break
 
 
 def read_excel(filepath: str, **kwargs: Any) -> pd.DataFrame:
@@ -24,20 +39,20 @@ def read_excel(filepath: str, **kwargs: Any) -> pd.DataFrame:
     na_values: str = kwargs.get("na_values", None)
     keep_default_na: str = kwargs.get("keep_default_na", None)
     skiprows: int = kwargs.get("skiprows", 0)
-    nrows: int = kwargs.get("nrows", 2)
+
+    nrows, offset = preview_args.get("nrows", 2), preview_args.get("offset", 0)
 
     try:
         wb = load_workbook(filepath, read_only=True)
         s = wb[sheet_name if sheet_name else wb.sheetnames[0]]
-        row_to_iterate = s.iter_rows(values_only=True)
+        if preview:
+            row_to_iterate = s.iter_rows(min_row=offset, max_row=offset + nrows, values_only=True)
+        else:
+            row_to_iterate = s.iter_rows(values_only=True)
     except InvalidFileException:
         wb = xlrd.open_workbook(filepath)
         s = wb[sheet_name if sheet_name else wb.sheet_names()[0]]
-        row_to_iterate = [s.row(rx) for rx in range(s.nrows)]
-
-    if preview:
-        nrows, offset = preview_args.get("nrows", 2), preview_args.get("offset", 0)
-        row_to_iterate = list(islice(row_to_iterate, offset, offset + nrows))
+        row_to_iterate = list(_yielder(preview, s, nrows, offset))
 
     row_subset = []
     row_number = 0
