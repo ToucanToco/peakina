c4c0801d81b810dccd91c174d81d1f68ba1d3210
sanix-darker <s4nixd@gmail.com>
2022-02-10 01:13:48 +0100

    feat: refacto readexcel

--- peakina/readers/excel.py
+++ peakina/readers/excel.py
@@ -2,7 +2,8 @@
 Module to add xml support
 """
 from io import StringIO
-from typing import Any, Dict
+from itertools import chain
+from typing import Any, Dict, List
 
 import pandas as pd
 import xlrd
@@ -10,53 +11,75 @@ from openpyxl import load_workbook
 from openpyxl.utils.exceptions import InvalidFileException
 
 
-def _yielder(preview: bool, s: Any, nrows: int = 2, offset: int = 0) -> Any:
+def _yielder(preview: bool, sheet_name: Any, nrows: int = 2, offset: int = 0) -> Any:
     """
     A generator for old excel types
     """
     if preview:
-        to_iter = (offset, offset + nrows)
+        to_iter = range(offset, offset + nrows)
     else:
-        to_iter = (0, s.nrows)
+        to_iter = range(sheet_name.nrows)
 
-    for rx in range(to_iter[0], to_iter[1]):
+    for rx in to_iter:
         try:
-            yield s.row(rx)
+            yield sheet_name.row(rx)
         except Exception:
             break
 
 
-def read_excel(filepath: str, **kwargs: Any) -> pd.DataFrame:
-    """
-    The read_excel function is using openpyxl to parse the csv file and read it
+def _read_old_xls_format(
+    wb: Any, sheetnames: List[Any], preview: bool = False, nrows: int = 2, offset: int = 0
+) -> Any:
+    """ """
+    sh_iter: Any = iter(())
 
-    """
-
-    preview: bool = kwargs.get("preview", False)
-    preview_args: Dict[str, Any] = kwargs.get("preview_args", {})
-
-    sheet_name: str = kwargs.get("sheet_name", None)
-    na_values: str = kwargs.get("na_values", None)
-    keep_default_na: str = kwargs.get("keep_default_na", None)
-    skiprows: int = kwargs.get("skiprows", 0)
-
-    nrows, offset = preview_args.get("nrows", 2), preview_args.get("offset", 0)
-
-    try:
-        wb = load_workbook(filepath, read_only=True)
-        s = wb[sheet_name if sheet_name else wb.sheetnames[0]]
+    for sh_name in sheetnames:
         if preview:
-            row_to_iterate = s.iter_rows(min_row=offset, max_row=offset + nrows, values_only=True)
-        else:
-            row_to_iterate = s.iter_rows(values_only=True)
-    except InvalidFileException:
-        wb = xlrd.open_workbook(filepath)
-        s = wb[sheet_name if sheet_name else wb.sheet_names()[0]]
-        row_to_iterate = list(_yielder(preview, s, nrows, offset))
+            sh_iter = chain(sh_iter, _yielder(preview, wb[1][sh_name], nrows, offset))
+
+        sh_iter = chain(sh_iter, _yielder(preview, wb[1][sh_name]))
+
+    return sh_iter
+
+
+def _read_new_xls_format(
+    wb: Any, sheetnames: List[Any], preview: bool = False, nrows: int = 2, offset: int = 0
+) -> Any:
+    """ """
+    sh_iter: Any = iter(())
+
+    for sh_name in sheetnames:
+        if preview:
+            sh_iter = chain(
+                sh_iter,
+                wb[1][sh_name].iter_rows(min_row=offset, max_row=offset + nrows, values_only=True),
+            )
+
+        sh_iter = chain(sh_iter, wb[1][sh_name].iter_rows(values_only=True))
+
+    return sh_iter
+
+
+def _get_row_to_iterate(
+    wb: Any, sheetnames: List[Any], preview: bool, nrows: int, offset: int, skiprows: int
+) -> Any:
+    """ """
+    if wb[0] == "new":
+        return _read_new_xls_format(wb, sheetnames, preview, nrows, offset)
+
+    return _read_old_xls_format(wb, sheetnames, preview, nrows, offset)
+
+
+def _read_sheets(
+    wb: Any, sheetnames: List[Any], preview: bool, nrows: int, offset: int, skiprows: int
+) -> List[Any]:
+    """ """
+    row_to_iterate = _get_row_to_iterate(wb, sheetnames, preview, nrows, offset, skiprows)
 
     row_subset = []
     row_number = 0
     for row in row_to_iterate:
+        row_number += 1
         if row_number < skiprows:
             continue
 
@@ -64,16 +87,46 @@ def read_excel(filepath: str, **kwargs: Any) -> pd.DataFrame:
         for cell in row:
             val = cell.value if type(cell) not in [str, int] else cell
             cells.append(str(val) if val else "")
+
         row_subset.append(f'{",".join(cells)}\n')
 
         if row_number == nrows:
             break
-        row_number += 1
+
+    if wb[0] == "new":
+        wb[1].close()
+
+    return row_subset
+
+
+def read_excel(
+    filepath: str,
+    *,
+    preview: bool = False,
+    preview_args: Dict[str, Any] = {},
+    sheet_name: str = "",
+    na_values: str = "",
+    keep_default_na: str = "",
+    skiprows: int = 0,
+) -> pd.DataFrame:
+    """
+    The read_excel function is using openpyxl to parse the csv file and read it
+
+    """
+    nrows, offset = preview_args.get("nrows", 2), preview_args.get("offset", 0)
 
     try:
-        wb.close()
-    except AttributeError:
-        pass
+        wb = ("new", load_workbook(filepath, read_only=True))
+    except InvalidFileException:
+        wb = ("old", xlrd.open_workbook(filepath))
+
+    sheetnames = (
+        [sheet_name]
+        if sheet_name and len(sheet_name)
+        else (wb[1].sheetnames if wb[0] == "new" else wb[1].sheet_names())
+    )
+
+    row_subset = _read_sheets(wb, sheetnames, preview, nrows, offset, skiprows)
 
     return pd.read_csv(
         StringIO("\n".join(row_subset)), na_values=na_values, keep_default_na=keep_default_na
