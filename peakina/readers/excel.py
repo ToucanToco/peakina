26eb479d575e6fe3b88686a9cdcb03e3af3e1a27
sanix-darker <s4nixd@gmail.com>
2022-02-11 10:55:14 +0100

    feat: updates from PR review

--- peakina/readers/excel.py
+++ peakina/readers/excel.py
@@ -3,12 +3,14 @@ Module to add xml support
 """
 import enum
 from io import StringIO
-from typing import Any, Dict, List
+from typing import Any, List, Optional
 
 import pandas as pd
 import xlrd
 from openpyxl import load_workbook
 from openpyxl.utils.exceptions import InvalidFileException
+
+from peakina.readers.csv import PreviewArgs
 
 
 class EXCEL_TYPE(enum.Enum):
@@ -16,36 +18,35 @@ class EXCEL_TYPE(enum.Enum):
     OLD = "old"
 
 
-def _yielder(preview: Dict[str, int], sheet_name: Any) -> Any:
-    """
-    A generator for old excel types
-    """
-
-    if bool(preview):
-        limit, offset = preview.get("nrows", 1), preview.get("offset", 0)
-        to_iter = range(offset, offset + limit + 1)
+def _read_old_xls_format(wb: Any, sh_name: str, preview: Optional[PreviewArgs]) -> Any:
+    """ """
+    if preview:
+        to_iter = range(
+            preview.get("offset", 0), preview.get("offset", 0) + preview.get("nrows", 500) + 1
+        )
     else:
-        to_iter = range(sheet_name.nrows)
+        to_iter = range(wb[sh_name].nrows)
 
     for rx in to_iter:
-        yield sheet_name.row(rx)
+        yield wb[sh_name].row(rx)
 
 
-def _read_old_xls_format(wb: Any, sh_name: str, preview: Dict[str, int]) -> Any:
-    """ """
-    return list(_yielder(preview, wb[sh_name]))
-
-
-def _read_new_xls_format(wb: Any, sh_name: str, preview: Dict[str, int]) -> Any:
-    """ """
-    limit, offset = preview.get("nrows", 500), preview.get("offset", 0)
-    return wb[sh_name].iter_rows(min_row=offset, max_row=offset + limit, values_only=True)
-
-
-def _get_row_to_iterate(wb: Any, excel_type: Any, sheet_name: str, preview: Dict[str, int]) -> Any:
+def _read_new_xls_format(wb: Any, sh_name: str, preview: Optional[PreviewArgs]) -> Any:
     """ """
 
-    if excel_type.value == "new":
+    return wb[sh_name].iter_rows(
+        min_row=preview.get("offset", 0) if preview else None,
+        max_row=preview.get("offset", 0) + preview.get("nrows", 500) if preview else None,
+        values_only=True,
+    )
+
+
+def _get_row_to_iterate(
+    wb: Any, excel_type: EXCEL_TYPE, sheet_name: str, preview: Optional[PreviewArgs]
+) -> Any:
+    """ """
+
+    if excel_type == EXCEL_TYPE.NEW:
         return _read_new_xls_format(wb, sheet_name, preview)
 
     return _read_old_xls_format(wb, sheet_name, preview)
@@ -53,9 +54,9 @@ def _get_row_to_iterate(wb: Any, excel_type: Any, sheet_name: str, preview: Dict
 
 def _read_sheets(
     wb: Any,
-    excel_type: Any,
+    excel_type: EXCEL_TYPE,
     sheetnames: List[Any],
-    preview: Dict[str, int],
+    preview: Optional[PreviewArgs],
     nrows: int,
     skiprows: int,
 ) -> List[Any]:
@@ -86,10 +87,10 @@ def _read_sheets(
             if row_number == nrows:
                 break
 
-    if excel_type.value == "new":
+    if excel_type == EXCEL_TYPE.NEW:
         wb.close()
 
-    # cleaning superflues rows with __sheet__
+    # cleaning extras rows with __sheet__
     if len(sheetnames) > 1:
         row_subset[1:] = [x for x in row_subset[1:] if "__sheet__" not in x]
 
@@ -99,9 +100,9 @@ def _read_sheets(
 def read_excel(
     filepath: str,
     *,
-    preview: Dict[str, int] = {},
+    preview: Optional[PreviewArgs] = None,
     sheet_name: str = "",
-    na_values: str = "",
+    na_values: Any = None,
     keep_default_na: Any = None,
     skiprows: int = 0,
     nrows: int = 50,
@@ -119,7 +120,7 @@ def read_excel(
     sheetnames = (
         [sheet_name]
         if sheet_name and len(sheet_name)
-        else (wb.sheetnames if excel_type.value == "new" else wb.sheet_names())
+        else (wb.sheetnames if excel_type == EXCEL_TYPE.NEW else wb.sheet_names())
     )
 
     row_subset = _read_sheets(wb, excel_type, sheetnames, preview, (nrows + 1), skiprows)
