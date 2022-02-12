283b1e231371725ee3555895ea5481a97e06bfd3
sanix-darker <s4nixd@gmail.com>
2022-02-12 15:04:46 +0100

    refacto: move the extract-columns because only used by read_csv

--- peakina/readers/common.py
+++ peakina/readers/common.py
@@ -2,8 +2,6 @@
 Common/Shared elements/classes between readers
 """
 
-from typing import List
-
 from pydantic.dataclasses import dataclass
 
 
@@ -11,8 +9,3 @@ from pydantic.dataclasses import dataclass
 class PreviewArgs:
     nrows: int = 500
     offset: int = 0
-
-
-def _extract_columns(filepath: str, encoding: str, sep: str) -> List[str]:
-    with open(filepath, buffering=10000, encoding=encoding) as ff:
-        return ff.readline().replace("\n", "").split(sep)
