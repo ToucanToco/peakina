7f27c60357e69057de69630206873cc21a3eb53d
Luc Léonard <github@luc-leonard.fr>
2022-02-18 13:58:20 +0100

    feat(pagination): coverage

--- tests/io/http/test_http_fetcher.py
+++ tests/io/http/test_http_fetcher.py
@@ -20,6 +20,24 @@ def test_http_fetcher(http_path, mocker):
     assert fetcher.mtime(http_path) == 457010553
 
 
+def test_http_fetcher_with_query_string(http_path, mocker):
+    http_path = http_path + "?foo=bar"
+    fetcher = HttpFetcher()
+    tmpfile = fetcher.open(http_path)
+    assert pd.read_csv(tmpfile).shape == (800, 13)
+    with pytest.raises(KeyError):
+        fetcher.mtime(http_path)
+
+    with pytest.raises(NotImplementedError):
+        fetcher.listdir(http_path)
+
+    stub_headers = {"last-modified": "Mon, 25 Jun 1984 11:22:33 GMT"}
+    mocker.patch.object(fetcher.pool_manager, "request").return_value.headers = stub_headers
+    assert fetcher.mtime(http_path) == 457010553
+
+    assert tmpfile.name.endswith(".csv")
+
+
 def test_http_mtime_error(mocker):
     """It should return None as default value in case of unexpected exception"""
     fetcher = HttpFetcher()
