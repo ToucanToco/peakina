f1a29bc43f7d2666747a4408e754dfcc94d3d005
Eric Jolibois <em.jolibois@gmail.com>
2022-02-21 21:53:39 +0100

    test: more explicit

--- tests/readers/test_csv.py
+++ tests/readers/test_csv.py
@@ -11,19 +11,45 @@ def test_simple_csv(path):
 
 def test_simple_csv_preview(path):
     """It should be able to get a preview of a csv file"""
+    ds = DataSource(path("fixture-1.csv"))
+    assert ds.get_df().shape == (12, 2)
+    assert ds.get_df().equals(
+        pd.DataFrame(
+            {
+                "month": [
+                    "Jan-14",
+                    "Fev-14",
+                    "Mars-14",
+                    "Avr-14",
+                    "Mai-14",
+                    "Juin-14",
+                    "Juil-14",
+                    "Aout-14",
+                    "Sept-14",
+                    "Oct-14",
+                    "Nov-14",
+                    "Dec-14",
+                ],
+                "value": [2.7, 3.2, 3.3, 3.1, 3.9, 3.4, 3.1, 3.2, 3.4, 3.8, 3.7, 3.6],
+            }
+        )
+    )
+
+    # preview with only `nrows`
+    ds = DataSource(
+        path("fixture-1.csv"),
+        reader_kwargs={"preview_nrows": 2},
+    )
+    assert ds.get_df().shape == (2, 2)
+    assert ds.get_df().equals(pd.DataFrame({"month": ["Jan-14", "Fev-14"], "value": [2.7, 3.2]}))
+
+    # preview with `offset` and `nrows`
     ds = DataSource(
         path("fixture-1.csv"),
         reader_kwargs={"preview_nrows": 2, "preview_offset": 2},
     )
     assert ds.get_df().shape == (2, 2)
-
     assert ds.get_df().equals(pd.DataFrame({"month": ["Mars-14", "Avr-14"], "value": [3.3, 3.1]}))
-
-    ds = DataSource(
-        path("fixture-1.csv"),
-        reader_kwargs={"preview_nrows": 2, "preview_offset": 0},
-    )
-    assert ds.get_df().shape == (2, 2)
 
 
 def test_simple_csv_metadata(path):
--- tests/readers/test_excel.py
+++ tests/readers/test_excel.py
@@ -5,17 +5,70 @@ from peakina import DataSource
 
 def test_simple_xls(path):
     """It should be able to detect type if not set"""
-    ds = DataSource(path("0_2.xls"), reader_kwargs={})
+    ds = DataSource(path("0_2.xls"))
     assert ds.get_df().shape == (2, 2)
 
 
 def test_simple_xls_preview(path):
     """It should be able to get a preview of an excel file"""
+    df = DataSource(path("fixture.xls")).get_df()
+    assert df.shape == (170, 6)
+    assert df[:5].equals(
+        pd.DataFrame(
+            {
+                "breakdown": ["Par territoire"] * 5,
+                "catégorie": ["Agglo 1 2014"] * 5,
+                "fréquence": [
+                    "Au moins 1 fois/mois",
+                    "plusieurs fois/an",
+                    "1 fois/an",
+                    "moins souvent",
+                    "jamais",
+                ],
+                "part": [9, 45, 35, 10, 1],
+                "clients": [896] * 5,
+                "pays": ["France"] * 5,
+            }
+        )
+    )
+
+    # preview with only `nrows`
+    ds = DataSource(
+        path("fixture.xls"),
+        reader_kwargs={"preview_nrows": 2},
+    )
+    assert ds.get_df().shape == (2, 6)
+    assert ds.get_df().equals(
+        pd.DataFrame(
+            {
+                "breakdown": ["Par territoire"] * 2,
+                "catégorie": ["Agglo 1 2014"] * 2,
+                "fréquence": ["Au moins 1 fois/mois", "plusieurs fois/an"],
+                "part": [9, 45],
+                "clients": [896] * 2,
+                "pays": ["France"] * 2,
+            }
+        )
+    )
+
+    # preview with `offset` and `nrows`
     ds = DataSource(
         path("fixture.xls"),
         reader_kwargs={"preview_nrows": 2, "preview_offset": 2},
     )
     assert ds.get_df().shape == (2, 6)
+    assert ds.get_df().equals(
+        pd.DataFrame(
+            {
+                "breakdown": ["Par territoire"] * 2,
+                "catégorie": ["Agglo 1 2014"] * 2,
+                "fréquence": ["1 fois/an", "moins souvent"],
+                "part": [35, 10],
+                "clients": [896] * 2,
+                "pays": ["France"] * 2,
+            }
+        )
+    )
 
 
 def test_simple_xls_metadata(path):
