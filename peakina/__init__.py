b67ac2ff3f8c661a9f095b41a3ca9d33cd4f0397
sanix-darker <s4nixd@gmail.com>
2022-02-28 15:14:13 +0100

    feat: get version from the package

--- peakina/__init__.py
+++ peakina/__init__.py
@@ -1,8 +1,12 @@
+import importlib.metadata as lib_meta
+
 from .cache import Cache, CacheEnum
 from .datapool import DataPool
 from .datasource import AVAILABLE_SCHEMES, DataSource, read_pandas
 from .helpers import TypeEnum
 from .io import MatchEnum, fetch
+
+__version__ = lib_meta.version(__package__ or __name__)
 
 __all__ = (
     "AVAILABLE_SCHEMES",
