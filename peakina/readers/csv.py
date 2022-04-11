d4ba31b930c8fb447bb6415f77b29bce2909b87d
David Nowinsky <david@nowinsky.net>
2022-04-11 17:02:23 +0200

    style(csv): style of reader_kwargs access

--- peakina/readers/csv.py
+++ peakina/readers/csv.py
@@ -90,7 +90,7 @@ def csv_meta(
 ) -> Dict[str, Any]:
     total_rows = _line_count(filepath_or_buffer, reader_kwargs.get("encoding"))
 
-    if not reader_kwargs.get("names") and (total_rows > 0):  # No header row
+    if "names" not in reader_kwargs and total_rows > 0:  # No header row
         total_rows = total_rows - 1
 
     if "nrows" in reader_kwargs:
