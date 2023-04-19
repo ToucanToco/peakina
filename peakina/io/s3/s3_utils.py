69cc3ec11f13fdd7b929ce387544f6c5f68de8fc
Luka Peschke <luka.peschke@toucantoco.com>
2023-04-19 10:03:33 +0200

    Context + coverage
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- peakina/io/s3/s3_utils.py
+++ peakina/io/s3/s3_utils.py
@@ -53,24 +53,23 @@ def parse_s3_url(url: str, file: bool = True) -> tuple[str | None, str | None, s
 
 
 def _s3_open_file_with_retries(fs: s3fs.S3FileSystem, path: str, retries: int) -> Any:
-    exception_err = None
-    for _ in range(retries):
+    nb_tries = 0
+    while nb_tries < retries:
         try:
-            logger.info(f"opening {path}")
+            logger.info(f"Opening {path}")
             file = fs.open(path)
             return file
         except Exception as ex:
-            exception_err = ex
-            logger.warning(f"could not open {path}: {ex}")
+            nb_tries += 1
+            if nb_tries >= retries:
+                raise Exception(f"Could not open {path} ({nb_tries} tries): {ex}") from ex
+            logger.warning(f"Could not open {path}: {ex}")
             # if the file has just been uploaded, then it might not be visible immediatly
             # but the fail to open has been cached by s3fs
             # so, we invalidate the cache
             fs.invalidate_cache(path)
             # and we give some time to S3 to settle the file status
             sleep(1)
-
-    logger.error(f"could not open {path}")
-    raise Exception(f"Unable to access the target file : {path}") from exception_err
 
 
 def s3_open(url: str, *, client_kwargs: dict[str, Any] | None = None) -> IO[bytes]:
