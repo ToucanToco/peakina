a698fc1e6bdba83b4758a184a4445bdf139b1aeb
Luka Peschke <luka.peschke@toucantoco.com>
2023-09-01 14:35:47 +0200

    please ?
    Signed-off-by: Luka Peschke <luka.peschke@toucantoco.com>

--- tests/io/ftp/test_ftp_fetcher.py
+++ tests/io/ftp/test_ftp_fetcher.py
@@ -7,14 +7,14 @@ from peakina.io.ftp import ftp_fetcher
 def test_ftp_fetcher(mocker: MockerFixture, ftp_path: str) -> None:
     ftp_fetcher.get_mtimes_cache.cache_clear()
     fetcher = ftp_fetcher.FTPFetcher()
-    mtime_spy = mocker.spy(ftp_fetcher, "ftp_mtime")
 
     myfile = "my_data_2015.csv"
     myfile_path = f"{ftp_path}/{myfile}"
     tmpfile = fetcher.open(myfile_path)
     assert pd.read_csv(tmpfile).shape == (2, 2)
 
-    mocker.patch("peakina.io.ftp.ftp_fetcher.get_mtimes_cache", return_value={"something": "else"})
+    mtime_spy = mocker.spy(ftp_fetcher, "ftp_mtime")
+    mocker.patch.object(ftp_fetcher, "get_mtimes_cache", return_value={"something": "else"})
     assert (mtime := fetcher.mtime(myfile_path)) is not None
     assert mtime > 1e9
     assert mtime_spy.call_count == 1
